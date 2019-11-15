5ba1fc5d3399c6ddf8be253b627fc03feaa85a6c
marcloh108 <marc.loh@molisys.com>
2019-11-15 17:05:19 +0700

    Write a program in C to add two numbers using pointers
    Create function to define and print
    
    Signed-off-by: marcloh108 <marc.loh@molisys.com>

--- question1/main.c
+++ question1/main.c
@@ -2,37 +2,22 @@
 
 int main()
 {
-    int m=300;
-    float fx=300.60;
-    char cht ='z';
+    int fno, sno, *ptr, *qtr, sum;
 
-    printf("\n\n Pointer : Demonstrate the use of & and * operator :\n");
-    printf("--------------------------------------------------------\n");
-
-    int *pt1;
-    float *pt2;
-    char *pt3;
-    pt1=&m;
-    pt2=&fx;
-    pt3=&cht;
-
-    printf(" m = %d\n",m);
-    printf(" fx = %f\n",fx);
-    printf(" cht = %c\n",cht);
-    printf("\n Using & operator :\n");
-    printf("-----------------------\n");
-    printf(" address of m = %p\n", &m);
-    printf(" address of fx = %p\n", &fx);
-    printf(" address of cht = %p\n", &cht);
-    printf("\n Using & and * operator :\n");
+    printf("\n\nPointer : Add two numbers :\n");
     printf("---------------------------------\n");
-    printf(" value at address of m = %d\n",*(&m));
-    printf(" value at address of fx = %f\n", *(&fx));
-    printf(" value at address of cht = %c\n", *(&cht));
-    printf("\n Using only pointer operator :\n");
-    printf("---------------------------------\n");
-    printf(" value at address of m = %d\n", *pt1);
-    printf(" value at address of fx = %f\n", *pt2);
-    printf(" value at address of cht = %c\n\n", *pt3);
+
+    printf(" Input the first number : ");
+    scanf("%d" , &fno);
+    printf(" Input the second number : ");
+    scanf("%d", &sno);
+
+    ptr = &fno;
+    qtr = &sno;
+
+    sum = *ptr + *qtr;
+
+    printf(" The sum of the external numbers is %d\n\n", sum);
+
     return 0;
 }
