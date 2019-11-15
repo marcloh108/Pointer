056627fa92fc44968f66cdba97c0f2cc98c40770
marcloh108 <marc.loh@molisys.com>
2019-11-15 16:37:52 +0700

    Write a program in C to demonstrate how to handle the pointers in the program
    Create function to define and print
    
    Signed-off-by: marcloh108 <marc.loh@molisys.com>

--- question1/main.c
+++ question1/main.c
@@ -2,19 +2,27 @@
 
 int main()
 {
-int m=10,n,o;
-int *z=&m ;
+    int* ab;
+    int m;
+    m = 29;
 
-    printf("\n\n Pointer : Show the basic declaration of pointer :\n");
-    printf("-------------------------------------------------------\n");
-    printf(" Here is m=10, n and o are two integer variable and *z is an integer");
-    printf("\n\n z stores the address of m  = %p\n",  z); // z is a pointer so %p would print the address
-    printf("\n *z stores the value of m = %d\n",   *z);
-    printf("\n &m is the address of m = %p\n",   &m); // &m gives the address of the integer variable m
-                             // so %p is the specifier for that address
-    printf("\n &n stores the address of n = %p\n",   &n);
-    printf("\n &o  stores the address of o = %p\n",   &o);
-    printf("\n &z stores the address of z = %p\n\n", &z); // &z gives the address, where the pointer z is
-                             // stored -> still an address -> %p is the right
-                             // specifier
+    printf("\n\n Pointer : How to handle the pointers in the program :\n");
+    printf("----------------------------------------------------------------\n");
+    printf(" Here is the declaration ab = int pointer, int m= 29");
+    printf("\n\n Address of m : %p\n",  &m); // z is a pointer so %p would print the address
+    printf("Value of m :%d\n",   m);
+    ab=&m;
+    printf("\n\n Now ab is assigned with address of m.\n");
+    printf("Address of pointer ab : %p\n", ab);
+    printf("Content of pointer ab : %d\n", *ab);
+    m = 34;
+    printf("\nThe value of m assigned to 34 now.\n");
+    printf("Address of pointer ab : %p\n",ab);
+    printf("Content of pointer ab : %d\n\n", *ab);
+    *ab=7;
+    printf("The pointer variable ab is assigned the value 7 now.\n");
+    printf("Address of m : %p\n", &m);
+
+    printf("Value of m : %d\n\n", m);
+    return 0;
 }
