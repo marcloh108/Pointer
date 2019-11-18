a8a056759f72455667d7cfe86148b41a7c721ace
marcloh108 <marc.loh@molisys.com>
2019-11-18 14:45:33 +0700

    Write a program in C to sort an array using Pointer
    Create function to define and print
    
    Signed-off-by: marcloh108 <marc.loh@molisys.com>

--- question1/main.c
+++ question1/main.c
@@ -1,26 +1,35 @@
 #include <stdio.h>
-
-int main()
+void main()
 {
-    char str1[50];
-    char *pt;
-    int ctrV, ctrC;
-    printf("Input a string: ");
-    fgets(str1, sizeof(str1), stdin);
+   int *a,i,j,tmp,n;
+    printf("\n\n Pointer : Sort an array using pointer :\n");
+    printf("--------------------------------------------\n");
 
-    pt = str1;
+   printf(" Input the number of elements to store in the array : ");
+   scanf("%d",&n);
 
-    ctrV = ctrC = 0;
-    while(*pt!='\0')
+   printf(" Input %d number of elements in the array : \n",n);
+   for(i=0;i<n;i++)
+      {
+      printf(" element - %d : ",i+1);
+      scanf("%d",a+i);
+      }
+   for(i=0;i<n;i++)
+   {
+    for(j=i+1;j<n;j++)
     {
-        if ( *pt == 'A'|| *pt == 'E' || *pt == 'I' || *pt == 'O' || *pt == 'U' || *pt == 'a' || *pt == 'e' || *pt == 'i' || *pt == 'o' || *pt == 'u')
-            ctrV++;
-        else
-            ctrC++;
-            pt++;
-
+       if( *(a+i) > *(a+j))
+       {
+      tmp = *(a+i);
+      *(a+i) = *(a+j);
+      *(a+j) = tmp;
+       }
     }
-    printf("Number of vowels : %d\nNumber of consonants : %d\n", ctrV,ctrC-1);
-   return 0;
-
+   }
+   printf("\n The elements in the array after sorting : \n");
+   for(i=0;i<n;i++)
+      {
+      printf(" element - %d : %d \n",i+1,*(a+i));
+      }
+printf("\n");
 }
