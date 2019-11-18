a4d59e50f2949fde84938de54576f86855bbede3
marcloh108 <marc.loh@molisys.com>
2019-11-18 16:30:16 +0700

    Write a program in C to show a pointer to an array which contents are pointer to structure
    Create function to define and print
    
    Signed-off-by: marcloh108 <marc.loh@molisys.com>

--- question1/main.c
+++ question1/main.c
@@ -1,19 +1,32 @@
 #include <stdio.h>
-union empAdd
+struct employee
 {
-char *ename;
-char stname[20];
-int pincode;
+char *empname;
+int empid;
 };
 
 int main()
 {
-    union empAdd employee,*pt;
-    employee.ename="Jhon Mc\0Donald";//assign  the string up to null character i.e. '\0'
+    printf("\n\n Pointer : Show a pointer to an array which contents are pointer to structure :\n");
+    printf("-----------------------------------------------------------------------------------\n");
 
-    pt=&employee;
+    static struct employee emp1={"Jhon",1001},emp2={"Alex",1002},emp3={"Taylor",1003};
+    struct employee(*arr[])={&emp1,&emp2,&emp3};
+    struct employee(*(*pt)[3])=&arr;
 
-    printf(" %s %s\n\n",pt->ename,(*pt).ename);
-
+    printf(" Employee Name : %s \n",(**(*pt+1)).empname);
+    printf("---------------- Explanation --------------------\n");
+    printf("(**(*pt+1)).empname\n");
+    printf("= (**(*&arr+1)).empname   as pt=&arr\n");
+    printf("= (**(arr+1)).empname     from rule *&pt = pt\n");
+    printf("= (*arr[1]).empname       from rule *(pt+i) = pt[i]\n");
+    printf("= (*&emp2).empname        as arr[1] = &emp2\n");
+    printf("= emp2.empname = Alex       from rule *&pt = pt\n\n");
+    printf(" Employee ID :  %d\n",(*(*pt+1))->empid);
+    printf("---------------- Explanation --------------------\n");
+    printf("(*(*pt+1))-> empid\n");
+    printf("= (**(*pt+1)).empid     from rule -> = (*).\n");
+    printf("= emp2.empid = 1002\n");
+    printf("\n\n");
     return 0;
 }
