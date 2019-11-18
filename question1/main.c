157c6f1c12841c1e0ef4783805dcc4f5d85b92b6
marcloh108 <marc.loh@molisys.com>
2019-11-18 15:50:02 +0700

    Write a program in C to show the usage of pointer to structure
    Create function for defining and printing
    
    Signed-off-by: marcloh108 <marc.loh@molisys.com>

--- question1/main.c
+++ question1/main.c
@@ -1,30 +1,14 @@
 #include <stdio.h>
+struct EmpAddress
+{
+char *ename;
+char stname[20];
+int pincode;
+}
+employee={"John Alter","Court Street \n",654134},*pt=&employee;
 
 int main()
 {
-    int arr1[10];
-    int i, n;
-    int *pt;
-
-    printf("Input the number of elements to store in the array (max 10) : ");
-    scanf("%d",&n);
-
-    printf("Input %d number of elements in the array : \n", n);
-    for(i=0;i<n;i++)
-    {
-        printf("element - %d : ", i+1);
-        scanf("%d", &arr1[i]);
-        pt++;
-    }
-
-    pt = &arr1[n-1];
-
-    printf("\n The elements of array in reverse order are :");
-
-    for(i = n; i > 0; i--)
-    {
-        printf("\n element - %d : %d ", i, *pt);
-        pt--;
-    }
-    printf("\n\n");
+    printf(" %s from %s \n\n",pt->ename,(*pt).stname);
+    return 0;
 }
