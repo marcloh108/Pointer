38f6aae35ee9f3ef2d7d043563c79827b68581de
marcloh108 <marc.loh@molisys.com>
2019-11-18 15:17:35 +0700

    Write a program in C to compute the sum of all elements in an array using pointers
    Create function to define and print
    
    Signed-off-by: marcloh108 <marc.loh@molisys.com>

--- question1/main.c
+++ question1/main.c
@@ -1,22 +1,27 @@
 #include <stdio.h>
-int* findLarger(int *num1, int *num2);
 
-int* findLarger(int *num1,int *num2)
-{
-    if(*num1 > *num2)
-    return num1;
-    else
-    return num2;
-}
 int main()
 {
-    int num1, num2;
-    int *result;
-    printf("Input the first number : ");
-    scanf("%d", &num1);
-    printf("Input the second number : ");
-    scanf("%d", &num2);
-    result=findLarger(&num1,&num2);
-            printf("The number %d is larger. ", *result);
+    int arr1[10];
+    int i, n, sum = 0;
+    int *pt;
 
+    printf("Input the number of elements to store in the array (max 10) : ");
+    scanf("%d",&n);
+
+    printf("Input %d number of elements in the array : \n", n);
+    for(i=0;i<n;i++)
+    {
+        printf("element - %d : ", i+1);
+        scanf("%d", &arr1[i]);
+    }
+
+    pt = arr1;
+
+    for(i = 0; i < n; i++)
+    {
+        sum = sum + *pt;
+        pt++;
+    }
+    printf("The sum of array is : %d\n\n", sum);
 }
