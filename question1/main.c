e767cf4ef93f0e9bcc6208dd88c36e9ec3add6ac
marcloh108 <marc.loh@molisys.com>
2019-11-18 09:41:30 +0700

    Write a program in C to add numbers using call by reference
    Create function for defining and printing
    
    Signed-off-by: marcloh108 <marc.loh@molisys.com>

--- question1/main.c
+++ question1/main.c
@@ -1,21 +1,23 @@
 #include <stdio.h>
+
+long addTwonumbers(long *n1, long *n2)
+{
+    long sum;
+    sum = *n1 + *n2;
+    return sum;
+}
 int main()
 {
-    long fno, sno;
+    long fno, sno, *ptr, *qtr, sum;
 
-    printf("\n\nPointer : Find the maximum number between two numbers\n");
+    printf("\n\nPointer : Add two numbers using call by reference:\n");
     printf("------------------------------------------------------\n");
 
     printf(" Input the first number : ");
     scanf("%ld" , &fno);
     printf(" Input the second number : ");
     scanf("%ld", &sno);
-
-    if ( fno > sno)
-    {
-        printf("%ld is the maximum number", fno);
-    }
-    else
-        printf("%ld is the maximum number", sno);
+    sum = addTwonumbers(&fno, &sno);
+    printf(" The sum of %ld and %ld is %ld\n\n", fno, sno, sum);
     return 0;
 }
